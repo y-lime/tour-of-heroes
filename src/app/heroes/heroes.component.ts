fd90db4657b577059fd540a260af5e8ca18356fe
y-lime <y.murai.tech@gmail.com>
2021-09-25 22:42:27 +0900

    section5: add navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(
     private heroService: HeroService,
@@ -19,11 +18,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
